b0c4031e2ca1f3c4da530b1516be79041968803c
0general <0general@naver.com>
2021-11-11 17:32:40 +0900

    LCS top-down memoization 코드 수정, 기존 코드는 복붙실수로 recursion으로 돌고 있었음.

--- week4/longest_common_subsequence.py
+++ week4/longest_common_subsequence.py
@@ -1,3 +1,5 @@
+import time
+
 
 def recursive_LCS(x, y, i, j):
     xlen = len(x)
@@ -15,23 +17,26 @@ def recursive_LCS(x, y, i, j):
         return max(recursive_LCS(x, y, i-1, j), recursive_LCS(x, y, i, j-1))
 
 
-def memo_LCS(x, y, i, j):  # top-down
+def memo_LCS(x, y, c, i, j):  # top-down
     xlen = len(x)
     ylen = len(y)
 
-    c = [[0]*ylen for _ in range(xlen)]
-    if i == 0 and j == 0:
-        return c[i][j] + (x[i] == y[j])
-    elif i == 0:
-        return c[i][j-1] + (x[i] == y[j])
-    elif j == 0:
-        return c[i-1][j] + (x[i] == y[j])
-    if x[i] == y[j]:
-        c[i][j] = recursive_LCS(x, y, i-1, j-1) + 1
-    else:
-        c[i][j] = max(recursive_LCS(x, y, i-1, j), recursive_LCS(x, y, i, j-1))
+    if c[i][j] == None:
+        if i == 0 and j == 0:
+            c[i][j] = int(x[i] == y[j])
+            return c[i][j]
+        elif i == 0:
+            c[i][j] = memo_LCS(x, y, c, i, j-1) + (x[i] == y[j])
+            return c[i][j]
+        elif j == 0:
+            c[i][j] = memo_LCS(x, y, c, i-1, j) + (x[i] == y[j])
+            return c[i][j]
+        if x[i] == y[j]:
+            c[i][j] = memo_LCS(x, y, c, i-1, j-1) + 1
+        else:
+            c[i][j] = max(memo_LCS(x, y, c, i-1, j), memo_LCS(x, y, c, i, j-1))
 
-    return c[-1][-1]
+    return c[i][j]
 
 
 def LCS(x, y):  # bottom-up using DP table
@@ -62,22 +67,28 @@ a = "BDCABA"
 b = "BCBDAB"
 n = len(a)-1
 m = len(b)-1
-print(recursive_LCS(a, b, n, m))
-print(memo_LCS(a, b, n, m))
-print(LCS(a, b))
+ans1 = recursive_LCS(a, b, n, m)
+c = [[None]*(len(b)) for _ in range(len(a))]
+ans2 = memo_LCS(a, b, c, n, m)
+ans3 = LCS(a, b)
+print(ans1, ans2, ans3)
 
 a = "IKNOWABOUTTHIS"
 b = "IGOTTHISTOO"
 n = len(a)-1
 m = len(b)-1
-print(recursive_LCS(a, b, n, m))
-print(memo_LCS(a, b, n, m))
-print(LCS(a, b))
+ans1 = recursive_LCS(a, b, n, m)
+c = [[None]*(len(b)) for _ in range(len(a))]
+ans2 = memo_LCS(a, b, c, n, m)
+ans3 = LCS(a, b)
+print(ans1, ans2, ans3)
 
 a = "AAAAAAAAABAAA"
 b = "AAAACAAAAAAAA"
 n = len(a)-1
 m = len(b)-1
-print(recursive_LCS(a, b, n, m))
-print(memo_LCS(a, b, n, m))
-print(LCS(a, b))
+ans1 = recursive_LCS(a, b, n, m)
+c = [[None]*(len(b)) for _ in range(len(a))]
+ans2 = memo_LCS(a, b, c, n, m)
+ans3 = LCS(a, b)
+print(ans1, ans2, ans3)
